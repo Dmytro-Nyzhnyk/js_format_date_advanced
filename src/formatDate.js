2b749d2b3569be2240f990a8c3f3b9e73c3dd7ca
Dmytro Nizhnyk <nothan89@gmail.com>
2025-04-26 14:42:45 +0300

    solution for js_format_date_advanced

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,72 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const arrFromDate = date.split(fromFormat[3]);
+  let year = '';
+  let month = '';
+  let day = '';
+  const reorderDateArr = [];
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    if (fromFormat[i] === 'YYYY') {
+      year += arrFromDate[i];
+    }
+
+    if (fromFormat[i] === 'YY') {
+      if (arrFromDate[i] < 30) {
+        year += '20' + arrFromDate[i];
+      }
+
+      if (arrFromDate[i] >= 30) {
+        year += '19' + arrFromDate[i];
+      }
+    }
+
+    if (fromFormat[i] === 'MM') {
+      month += arrFromDate[i];
+    }
+
+    if (fromFormat[i] === 'DD') {
+      day += arrFromDate[i];
+    }
+  }
+
+  for (let j = 0; j < toFormat.length; j++) {
+    if (toFormat[j] === 'YYYY') {
+      reorderDateArr[j] = year;
+    }
+
+    if (toFormat[j] === 'YY') {
+      reorderDateArr[j] = year.slice(2);
+    }
+
+    if (toFormat[j] === 'MM') {
+      reorderDateArr[j] = month;
+    }
+
+    if (toFormat[j] === 'DD') {
+      reorderDateArr[j] = day;
+    }
+  }
+
+  const result = reorderDateArr.join(toFormat[3]);
+
+  return result;
 }
+
+formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['YYYY', 'MM', 'DD', '.']);
+// '2020.02.18'
+
+formatDate('2020-02-18', ['YYYY', 'MM', 'DD', '-'], ['DD', 'MM', 'YYYY', '.']);
+// '18.02.2020'
+
+formatDate('18-02-2020', ['DD', 'MM', 'YYYY', '-'], ['DD', 'MM', 'YY', '/']);
+// '18/02/20'
+
+formatDate('20/02/18', ['YY', 'MM', 'DD', '/'], ['YYYY', 'MM', 'DD', '.']);
+// '2020.02.18'
+
+formatDate('97/02/18', ['YY', 'MM', 'DD', '/'], ['DD', 'MM', 'YYYY', '.']);
+// '18.02.1997'
 
 module.exports = formatDate;
